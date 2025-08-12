edbddca0f91902dca742b43f5e8f75693c337382
Beginner090902 <milibo45@gmail.com>
2025-08-12 16:42:58 +0200

    add Main Menü and changed the scene

---
+++ Sinus-Simulation/Sinus-Wellen-Simulaton.gd
@@ -0,0 +1,18 @@
+extends Node
+#generel 
+var num_points  := 400
+var abstand := 0.5
+var geschwindigkeit := 2
+
+#Sinus1
+var amplitude1 := 50.0
+var frequency1 := 20.0
+var offset_x1 := 0.0
+var offset_y1 := 0.0
+
+
+#sinus2
+var amplitude2 := 50.0
+var frequency2 := 1.5
+var offset_x2 := 0.0
+var offset_y2 := 0.0
---
+++ Sinus-Simulation/sinus_2.gd
@@ -0,0 +1,47 @@
+extends Line2D
+
+var num_points  = SinusWellen.num_points
+var abstand  = SinusWellen.abstand
+var geschwindigkeit  = SinusWellen.geschwindigkeit
+
+var bewegungs_geschwindigkeit: float
+
+func _ready() -> void:
+	# Lösche vorherige Punkte, falls vorhanden
+	clear_points()
+	start_line()
+
+func start_line():
+	for i in range(num_points):
+		add_point(Vector2(i * abstand,0))
+
+		
+		
+func _process(delta: float) -> void:
+	var amplitude = SinusWellen.amplitude2
+	var frequency = SinusWellen.frequency2
+	var offset_x = SinusWellen.offset_x2
+	var offeset_y = SinusWellen.offset_y2
+	bewegungs_geschwindigkeit += geschwindigkeit * delta
+	
+	for i in range(num_points - 1):
+		var next_pos = get_point_position(i + 1)
+		set_point_position(i, Vector2(next_pos.x - abstand, next_pos.y))
+	
+	var y = amplitude * sin(frequency * bewegungs_geschwindigkeit + offset_x) + offeset_y
+	set_point_position(num_points-1, Vector2(num_points* abstand +1, y))
+
+
+func _on_amp_eingabe_text_changed(new_text: String) -> void:
+	SinusWellen.amplitude2 = float(new_text)
+
+
+func _on_fre_eingabe_text_changed(new_text: String) -> void:
+	SinusWellen.frequency2 = float(new_text)
+
+func _on_xver_eingabe_text_changed(new_text: String) -> void:
+	SinusWellen.offset_x2 = float(new_text)
+
+
+func _on_yver_eingabe_text_changed(new_text: String) -> void:
+	SinusWellen.offset_y2 = float(new_text)
